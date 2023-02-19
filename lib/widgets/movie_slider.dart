51dcc21b40c9957f118ed938ae0f64259b8ba103
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-19 18:16:09 -0300

    Finalizar el MovieSlider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -8,7 +8,7 @@ class MovieSlider extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 250,
-      color: Colors.red,
+      //color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         //mainAxisAlignment: MainAxisAlignment.center,
@@ -17,6 +17,8 @@ class MovieSlider extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Text('Populares')
           ),
+
+          const SizedBox(height: 5,),
 
           Expanded(
             child: ListView.builder(
@@ -39,8 +41,33 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      //color: Colors.green,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-details'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5,),
+          const Text(
+            'Star Wars: El retorno del dsfsdfdsdfddsdfsdfdsf',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+
+        ],
+      ),
     );
   }
 }
